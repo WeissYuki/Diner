68a95e62dac9a85e7ea270ac51ef1ffd76f15d86
WeissYuki <fatheen.rizal@gmail.com>
2024-10-30 20:55:08 +0100

    better wall movement

--- diner-queerer/wall_prototype.gd
+++ diner-queerer/wall_prototype.gd
@@ -1,15 +1,21 @@
 extends RigidBody3D
 @export var SpeedX:float
 @export var SpeedY:float
-var scaler = 1
+@export var StartSizeX:float
+@export var StartSizeY:float
+@export var scalerX:float
+@export var scalerY:float
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$CollisionShape3D.shape.size.x = StartSizeX
+	$MeshInstance3D.mesh.size.x = StartSizeX 
+	$CollisionShape3D.shape.size.y = StartSizeY
+	$MeshInstance3D.mesh.size.y = StartSizeY 
 
 
 func _physics_process(delta):
-	scale.y = scaler
-	scaler-=0.0005
+	$CollisionShape3D.shape.size.y -= scalerY
+	$MeshInstance3D.mesh.size.y -= scalerY
 	add_constant_force(Vector3(SpeedX*0.001, SpeedY*0.001, 0))
 	
