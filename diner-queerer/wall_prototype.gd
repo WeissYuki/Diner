34b24c851687e1991956ca2695d74742115285f5
WeissYuki <fatheen.rizal@gmail.com>
2024-11-03 14:17:11 +0100

    found and finally fixed a stupid bug with the walls, hacksawed a solution so bad it makes me want to vomit

--- diner-queerer/wall_prototype.gd
+++ diner-queerer/wall_prototype.gd
@@ -8,14 +8,16 @@ extends RigidBody3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$CollisionShape3D.shape.size.x = StartSizeX
-	$MeshInstance3D.mesh.size.x = StartSizeX 
-	$CollisionShape3D.shape.size.y = StartSizeY
-	$MeshInstance3D.mesh.size.y = StartSizeY 
+	$CollisionShape3D.shape.size.x = self.StartSizeX
+	$MeshInstance3D.mesh.size.x = self.StartSizeX 
+	$CollisionShape3D.shape.size.y = self.StartSizeY
+	$MeshInstance3D.mesh.size.y = self.StartSizeY 
 
 
 func _physics_process(delta):
 	$CollisionShape3D.shape.size.y -= scalerY
 	$MeshInstance3D.mesh.size.y -= scalerY
+	$CollisionShape3D.shape.size.x -= scalerX
+	$MeshInstance3D.mesh.size.x -= scalerX
 	add_constant_force(Vector3(SpeedX*0.001, SpeedY*0.001, 0))
 	
